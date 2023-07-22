b8849309e9c9d87db55a4b89c5ed71506b5fd6a3
kujiraf <frkw123@gmail.com>
2023-07-22 14:02:36 +0000

    add ssm agent

--- terraform/eks/nodes.tf
+++ terraform/eks/nodes.tf
@@ -18,7 +18,8 @@ module "eks-node-role" {
   policies = [
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   ]
 }
 
